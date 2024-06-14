eacc7ecd4a36a75ba7c96b621f3d889c1e853086
DmitriyKost <dima3588858@gmail.com>
2024-06-14 19:50:28 +0300

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use askama_axum::IntoResponse;
-use axum::{routing::get, Router};
+use axum::{http::{header::ACCESS_CONTROL_ALLOW_ORIGIN, HeaderValue}, routing::get, Router};
+use tower_http::set_header::response::SetResponseHeaderLayer;
 use askama::Template;
+
 
 #[tokio::main()]
 async fn main() {
-    let router = Router::new().route("/", get(home_page));
+    let router = Router::new().
+        route("/", get(home_page))
+        .layer(SetResponseHeaderLayer::if_not_present(
+            ACCESS_CONTROL_ALLOW_ORIGIN,
+            HeaderValue::from_static("*"),
+        ));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:6969").await.unwrap();
     axum::serve(listener, router).await.unwrap()
 }
